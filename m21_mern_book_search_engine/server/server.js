0eedb87097449f7a64603aebc03bade84103c030
Edward Vonschondorf <edvonschondorf@gmail.com>
2023-11-25 14:52:06 -0800

    Implement apollo server in server js

--- m21_mern_book_search_engine/server/server.js
+++ m21_mern_book_search_engine/server/server.js
@@ -1,15 +1,16 @@
-// TODO: Implement the Apollo Server and apply it to the Express server as middleware
-
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-
+// const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 const startApolloServer = async () => {
   await server.start();
