170f3e3e34f7226650c52c001c537ae52ab19ac5
Edward Vonschondorf <edvonschondorf@gmail.com>
2023-11-26 12:37:37 -0800

    Convert auth middleware from REST to GraphQL implementation

--- m21_mern_book_search_engine/server/server.js
+++ m21_mern_book_search_engine/server/server.js
@@ -5,11 +5,13 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware
 });
 
 const app = express();
